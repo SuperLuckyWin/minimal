c9972be5e2be50b1d1435c8fd6d06a7a1b62e957
SuperLuckyWin <yunbarbalichcxc31@gmail.com>
2025-01-13 17:33:52 +0800

    Create config.py

---
+++ trading_system/config.py
@@ -0,0 +1,15 @@
+OKX_API_KEY = "330337d2-f6cc-437b-bf90-cdf06767d470"
+OKX_API_SECRET = "A58E52B7E0C2B7FEFBAF025ECF5F5E41"
+OKX_API_PASSPHRASE = "Zxc123123-"
+
+OKX_BASE_URL = "https://www.okx.com"
+OKX_WS_URL = "wss://ws.okx.com:8443/ws/v5/public"
+OKX_PRIVATE_WS_URL = "wss://ws.okx.com:8443/ws/v5/private"
+
+# SYMBOL = "BTC-USDT-SWAP" # Removed, will be dynamically populated
+INTERVAL = '1m'
+TRADE_QTY = 0.001  # 合约交易数量，根据实际情况修改
+NEWS_SOURCE = "https://www.coindesk.com/feed/"
+KEYWORDS = ["bitcoin", "ethereum", "crypto"]
+TOP_N_VOLUME = 50 # 前 50 名币种
+MAX_SYMBOLS = 3 # 最大同时交易的币种数量
